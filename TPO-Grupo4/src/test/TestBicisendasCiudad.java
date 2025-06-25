f1fbfea8527659159e687af63a95ccb0d4a8a0ab
gachu <gaelkugler@gmail.com>
2025-06-25 18:11:39 -0300

    Cambios en el orden de los nodos

--- TPO-Grupo4/src/test/TestBicisendasCiudad.java
+++ TPO-Grupo4/src/test/TestBicisendasCiudad.java
@@ -25,14 +25,10 @@ public class TestBicisendasCiudad {
         System.out.println("Ingrese el peso entre 2 y 3:");
         int p23 = scanner.nextInt();
         mapa.agregarArista(2, 3, p23);
-
-        System.out.println("Ingrese el peso entre 2 y 4:");
-        int p24 = scanner.nextInt();
-        mapa.agregarArista(2, 4, p24);
         
-        System.out.println("Ingrese el peso entre 1 y 3:");
-        int p13 = scanner.nextInt();
-        mapa.agregarArista(1, 3, p13);
+        System.out.println("Ingrese el peso entre 1 y 4:");
+        int p14 = scanner.nextInt();
+        mapa.agregarArista(1, 4, p14);
 
         System.out.println("Ingrese el peso entre 3 y 4:");
         int p34 = scanner.nextInt();
